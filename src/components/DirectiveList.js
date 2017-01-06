ddb13c84a5d2b70d7806a4ed123e84432ce6598d
Erin Bond <erin@erinebond.com>
2017-01-06 14:54:04 -0800

    Hardcoded directives now show up on the screen of the simulator

--- src/components/DirectiveList.js
+++ src/components/DirectiveList.js
@@ -1,37 +1,45 @@
 //DirectiveList.js
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import axios from 'axios';
 import DirectiveDetail from './DirectiveDetail';
 
 class DirectiveList extends Component {
 
     state = { directives: [] }; //initial or empty state, property of this
-    //this.state.albums would return an empty array
+    //this.state.directives would return an empty array
 
     componentWillMount (){
-      console.log('component will mount in directive list')
-      console.log('this.state.directives', this.state.directives)
-      axios.get('https://treasure-chest-api.herokuapp.com/').then(function (response) {
-        console.log(response.data.directives);
-        state = { directives: response.data.directives }
-
-        console.log('this.state.directives', this.state.directives)
-      })
+      //console.log('component will mount in directive list')
+      //console.log('this.state.directives', this.state.directives)
+      axios.get('https://treasure-chest-api.herokuapp.com/').then( response => this.setState( { directives: response.data.directives })) .catch(function (error) {
+    console.log(error);
+    });;
     }
 
     renderDirectives() {
-      console.log('renderingDirectives() in DirectiveList')
-      return this.state.directives
-      // return this.state.directives.map(directive =>
-      //   <DirectiveDetail key={ directive.name } directive = {directive} />);
+      console.log('renderingDirectives() in DirectiveList');
+      console.log('this.state.directives', this.state.directives);
+      console.log('this.state.directives[0]', this.state.directives[0]);
+
+      if (typeof this.state.directives[0] !== 'undefined')  {
+        console.log('this.state.directives[0].name', this.state.directives[0].name)
+
+        return this.state.directives.map(directive =>
+          <Text> { directive.name } </Text>);
+      }
+
+
+
     }
 
-    render(){
+    render() {
+      console.log(this.state);
+
       return (
         <View>
-          {this.renderDirectives()}
+          { this.renderDirectives() }
         </View>
       );
     }
