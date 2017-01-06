94f8aed1f6923e8cd6c8a7713297186bb243d735
Erin Bond <erin@erinebond.com>
2017-01-06 10:54:24 -0800

    Welcome screen displays Scavenger Hunt Capstone in simulator, installed axios

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
 
 import React, { Component } from 'react';
 import {
@@ -11,20 +7,14 @@ import {
   Text,
   View
 } from 'react-native';
+import axios from 'axios';
 
 export default class hunt extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+           Scavenger Hunt Capstone!
         </Text>
       </View>
     );
@@ -42,11 +32,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
