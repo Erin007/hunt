6bc134cebd52a368aa14f8c153d1f30bf0168490
Erin Bond <erin@erinebond.com>
2017-01-08 20:20:02 -0800

    Made app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+//app.js
+
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component {
+  render(){
+    return (
+      <View>
+        <Text> The hunt is on! </Text>
+      </View>
+    );
+  }
+}
