97856b77d940625c96b0f61b8ee2bc7658fc5fd8
Erin Bond <erin@erinebond.com>
2017-01-09 21:38:37 -0800

    Handle authentication cases where the user enters an incorrect email or password combination or doesn't yet have an account

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,10 +5,26 @@ import Button from './common/Button';
 import Card from './common/Card';
 import CardSection from './common/CardSection';
 import Input from './common/Input';
+import firebase from 'firebase';
+import { Text } from 'react-native';
 
 class LoginForm extends Component {
 
-  state = { email: '', password: ''}
+  state = { email: '', password: '', error: ''}
+
+  onButtonPress(){
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      //if signin doesn't work
+      .catch(() =>{
+        //try to make an account for the user
+        firebase.auth().createUserWithEmailAndPassword(email,password)
+          .catch(() => {
+            this.setState({ error: 'Authenication Failed'})
+          });
+      });
+  }
 
   render(){
     return (
@@ -30,11 +46,23 @@ class LoginForm extends Component {
             onChangeText = {password => this.setState({ password })}/>
         </CardSection>
 
+        <Text style= {styles.errorTextStyle}>
+          { this.state.error }
+        </Text>
+
         <CardSection>
-          <Button> Log in! </Button>
+          <Button onPress={this.onButtonPress.bind(this)}> Log in! </Button>
         </CardSection>
       </Card>
     )
+  }
+};
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 };
 
